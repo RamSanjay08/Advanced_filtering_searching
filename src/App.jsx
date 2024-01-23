ca6bb8e0246657b21a4d25c72fd5de8475613a04
Ram Sanjay <ramsanjaydev08@gmail.com>
2024-01-23 10:41:17 +0530

    files ready to export

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Navbar/Navbar'
+import Products from './Products/Products'
+import Recommended from './Recommended/Recommended'
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Products />
+      <Recommended />
+    </>
+  )
+}
+
+export default App
