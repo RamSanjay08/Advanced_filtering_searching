bbf065b6a720cde60db4d37fc45238c75425bd8f
Ram Sanjay <ramsanjaydev08@gmail.com>
2024-01-24 09:33:16 +0530

    sidebar added

--- src/Sidebar/Colors/Colors.jsx
+++ src/Sidebar/Colors/Colors.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import "../Category/category.css";
 
 const Colors = () => {
   return (
-    <div>Colors</div>
-  )
-}
+    <div className="category_container">
+      <h3>Colors</h3>
+      <div className="checkbox_container">
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>All</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>Black</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>Blue</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>Red</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>Green</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>White</span>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Colors
+export default Colors;
--- src/Sidebar/Prices/Prices.jsx
+++ src/Sidebar/Prices/Prices.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import "../Category/category.css";
 
 const Prices = () => {
   return (
-    <div>Prices</div>
-  )
-}
+    <div className="category_container">
+      <h3>Prices</h3>
+      <div className="checkbox_container">
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>All</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>$0 - 50</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>$50 - 100</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>$100 - 150</span>
+          </label>
+        </div>
+        <div className="label_container">
+          <label className="category_label">
+            <input type="radio" name="test" className="category_input" />{" "}
+            <span>Over - $150</span>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Prices
+export default Prices;
